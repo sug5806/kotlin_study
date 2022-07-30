4a019af4487b6f15dbfbfb22952ea2a6ac15416a
홍동수 <ds.h@purple.io>
2022-07-30 11:26:06 +0900

    Exception 예제

--- src/main/kotlin/Exception.kt
+++ src/main/kotlin/Exception.kt
@@ -18,11 +18,24 @@ fun main() {
     }
 
     // try-catch 는 표현식 이므로 값을 리턴할 수 있음.
-    val a = try{
+    val a = try {
         "1234".toInt()
     } catch (e: Exception) {
         println("예외 발생!")
     }
     println(a)
 
+    // 강제 예외 발생
+//    throw Exception("예외 발생!")
+
+    // 값 리턴하기
+    val b = failFast("예외 발생! 값 념겨주기")
+
+
+
+}
+
+// Nothing은 함수가 리턴될일이 없거나 예외를 던지는 경우 사용한다.
+fun failFast(message: String): Nothing {
+    throw java.lang.IllegalArgumentException(message)
 }
